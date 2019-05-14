d2f5910e28e1543d77874dff9394ed87ffe801e0
test <alexey.varyzgin@intel.com>
2019-05-14 17:19:20 +0300

    [FORK][FEATURE] TBB_AUTO was enabled
    (cherry picked from commit 6cc5b1d1abcef23a342ba9ee1d1c6cc0881cddee)

--- src/common/dnnl_thread.hpp
+++ src/common/dnnl_thread.hpp
@@ -69,7 +69,7 @@ inline void dnnl_thr_barrier() {
 #pragma omp barrier
 }
 
-#elif DNNL_CPU_THREADING_RUNTIME == DNNL_RUNTIME_TBB
+#elif (DNNL_CPU_THREADING_RUNTIME == DNNL_RUNTIME_TBB || DNNL_CPU_THREADING_RUNTIME == DNNL_RUNTIME_TBB_AUTO)
 
 #include "common/dnnl_thread_tbb_proxy.hpp"
 
