113a0cf5456098ce53eb22bf74a194561b458462
Anton Voronov <anton.voronov@intel.com>
2023-07-14 20:25:33 +0000

    fork: feature: enable FP16 reorder on AVX2

--- src/cpu/x64/jit_uni_reorder.cpp
+++ src/cpu/x64/jit_uni_reorder.cpp
@@ -200,7 +200,7 @@ struct jit_uni_reorder_kernel_f32_t : public kernel_t, public jit_generator_t {
                 && IMPLICATION(utils::one_of(bf16, p.itype, p.otype),
                         mayiuse(avx512_core) || mayiuse(avx2_vnni_2))
                 && IMPLICATION(utils::one_of(f16, p.itype, p.otype),
-                        mayiuse(avx512_core_fp16) || mayiuse(avx2_vnni_2))
+                        mayiuse(avx512_core_fp16) || mayiuse(avx2))
                 && IMPLICATION(utils::one_of(f8_e5m2, p.itype, p.otype)
                                 || utils::one_of(f8_e4m3, p.itype, p.otype),
                         (mayiuse(avx512_core_fp16) || mayiuse(avx10_2)))
@@ -303,7 +303,7 @@ struct jit_uni_reorder_kernel_f32_t : public kernel_t, public jit_generator_t {
                             vcvtph2psx(dst, src);
                         else
                             vcvtph2psx(dst, Xmm(src.getIdx()));
-                    } else if (is_superset(isa_, avx2_vnni_2)) {
+                    } else if (is_superset(isa_, avx2)) {
                         if (src.isMEM())
                             vcvtph2ps(dst, src);
                         else
@@ -2838,12 +2838,14 @@ status_t jit_blk_reorder_t::pd_t::create(reorder_pd_t **reorder_pd,
     if (prb.ndims > 1) {
         int batch_idx = prb.nodes[0].is > prb.nodes[1].is ? 0 : 1;
         int channel_idx = batch_idx == 0 ? 1 : 0;
-        batch_strided_input =
-                (ptrdiff_t) prb.nodes[channel_idx].n * prb.nodes[channel_idx].is < prb.nodes[batch_idx].is;
+        batch_strided_input = (ptrdiff_t)prb.nodes[channel_idx].n
+                        * prb.nodes[channel_idx].is
+                < prb.nodes[batch_idx].is;
         batch_idx = prb.nodes[0].os > prb.nodes[1].os ? 0 : 1;
         channel_idx = batch_idx == 0 ? 1 : 0;
-        batch_strided_output =
-                (ptrdiff_t) prb.nodes[channel_idx].n * prb.nodes[channel_idx].is < prb.nodes[batch_idx].is;
+        batch_strided_output = (ptrdiff_t)prb.nodes[channel_idx].n
+                        * prb.nodes[channel_idx].is
+                < prb.nodes[batch_idx].is;
     }
 
     prb_tile_normalize(prb);
