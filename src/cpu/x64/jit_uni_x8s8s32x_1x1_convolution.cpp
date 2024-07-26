9b1cd7c46724e45426d081ce7f1100e9cf63b1a1
Luwei Zhou <luwei.zhou@intel.com>
2024-07-26 10:11:05 +0200

    [FORK][FEATURE] Migrate legacy post ops and zero points on runtime data pointers
    ONEDNN 3.2 migration squashed commits:
        - fix depthwise nwc conv
        - Fix deconv 3D post OPs segment fault issue. (#130)
        - fix in avx2 conv+fakequant post ops +nxc last channel wrong result (#128)
        - Luwei/fix deconv 3d postops bug (#136)
            -- Fix the deconv fused with depthwise issue in cpuFuncTests
            -- Switch to use jit_uni_depthwise_injector API.
            -- Fix potential conflicts in registers and YMM.
            -- Update with optimization.
        - fix legacyOps with stock src_zero_point in jit_avx512_core_amx
        - Fix incorrect offset to rsp
        - Preserve bf16emu scratch register when conflict with legacy post ops
        - fix per-OC legacyPostOps for jit_avx512_dw_conv_fwd_kernel_bf16
        - Fix segment fault caused by dest scale.
    
    ONEDNN 3.5 migration squshed commmits:
    [FIX][FORK][FEATURE] Introduced Depthwise and Quantization post ops
    
    [FORK] [FIX] Fix legacy zero point issue on AVX2 + AVX512.
    [FEATURE]Migrate legacy post ops and zero points on runtime data pointers
    
    [FORK][FIX] jit_uni_dw_conv_row_f32: fixed post ops start idx
    [FEATURE] Migrate legacy post ops and zero points on runtime data pointers
    
    [Fix] Update the attr checking caused by forked onednn.
    
    [FORK][FIX][x64] Add proper post op checks to gemm_conv is split
    particially and squashed.
    
    [FORK][FIX] jit_uni_dw_conv_kernel_f32: fixed register conflict
    
    [FIX] SPlit the removed unused variables
    
    [FIX] fix avx512 bf16 dw stack pointer
    
    [ARM] Fixed legacy post-ops changes for ARM target
    
    [FORK][FIX] fix args checking issue
    
    Signed-off-by: HU Yuan2 <yuan2.hu@intel.com>
    (cherry picked from commit 7f4f2e07942692242e7a58e29558f09eee3fe136)

--- src/cpu/x64/jit_uni_x8s8s32x_1x1_convolution.cpp
+++ src/cpu/x64/jit_uni_x8s8s32x_1x1_convolution.cpp
@@ -74,13 +74,16 @@ status_t jit_uni_x8s8s32x_1x1_convolution_fwd_t<isa>::execute_forward(
     const void *dw_dst_scales = CTX_IN_MEM(const void *,
             DNNL_ARG_ATTR_SCALES | DNNL_ARG_ATTR_POST_OP_DW | DNNL_ARG_DST);
 
+    DEFINE_OUTPUT_COMPENSATION_BUFFER(output_compensation, pd()->jcp_);
+
     parallel(pd()->jcp_.nthr, [&](const int ithr, const int nthr) {
         execute_forward_thr(ithr, nthr, src, weights, bias, weights_dw, bias_dw,
                 dst, src_scales, wei_scales, dst_scales, dw_wei_scales,
                 dw_dst_scales, src_zero_points, dst_zero_points,
                 ctx.get_scratchpad_grantor(),
                 post_ops_binary_rhs_arg_vec.data(),
-                post_ops_binary_rhs_arg_vec_dw.data());
+                post_ops_binary_rhs_arg_vec_dw.data(),
+                output_compensation);
     });
     return status::success;
 }
@@ -95,7 +98,8 @@ void jit_uni_x8s8s32x_1x1_convolution_fwd_t<isa>::execute_forward_thr(
         const int32_t *dst_zero_points,
         const memory_tracking::grantor_t &scratchpad,
         const void *post_ops_binary_rhs_arg_vec,
-        const void *post_ops_binary_rhs_arg_vec_dw) const {
+        const void *post_ops_binary_rhs_arg_vec_dw,
+        const int32_t *output_compensation) const {
     const memory_desc_wrapper src_d(pd()->src_md());
     const memory_desc_wrapper dst_d(pd()->dst_1x1_md());
     const memory_desc_wrapper weights_d(pd()->weights_md(0));
@@ -124,7 +128,7 @@ void jit_uni_x8s8s32x_1x1_convolution_fwd_t<isa>::execute_forward_thr(
     auto offset = weights_d.size() - weights_d.additional_buffer_size();
     char *w = const_cast<char *>(weights);
     const int32_t *compensation = (jcp.signed_input) ? reinterpret_cast<int32_t *>(w + offset) :
-                                  (jcp.with_input_zp) ? pd()->attr()->output_compensations_.shifts_ : nullptr;
+                                  (jcp.with_input_zp) ? output_compensation : nullptr;
     const int32_t *zp_compensation = jcp.src_zero_point
             ? reinterpret_cast<int32_t *>(&w[offset])
                     + (jcp.signed_input ? jcp.ngroups * jcp.oc : 0)
