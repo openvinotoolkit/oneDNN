2b484815778e793afcd37289aef6aae17b7964b8
Egor Duplensky <egor.duplensky@gmail.com>
2025-04-16 19:23:29 +0200

    [CPU] Avoid xbyak redefinitions

--- src/cpu/x64/cpu_isa_traits.hpp
+++ src/cpu/x64/cpu_isa_traits.hpp
@@ -28,8 +28,12 @@
 
 #include "cpu/platform.hpp"
 
+#if !defined(XBYAK64)
 #define XBYAK64
+#endif
+#if !defined(XBYAK_NO_OP_NAMES)
 #define XBYAK_NO_OP_NAMES
+#endif
 /* in order to make selinux happy memory that would be marked with X-bit should
  * be obtained with mmap */
 // #define XBYAK_USE_MMAP_ALLOCATOR
@@ -40,7 +44,9 @@
 // #undef XBYAK_NO_EXCEPTION
 // #endif
 #ifdef DNNL_XBYAK_NO_EXCEPTION
+#if !defined(XBYAK_NO_EXCEPTION)
 #define XBYAK_NO_EXCEPTION
+#endif
 #endif
 #if defined(_MSC_VER) && !defined(__INTEL_COMPILER)
 /* turn off `size_t to other-type implicit casting` warning
