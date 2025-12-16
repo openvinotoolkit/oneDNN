ed54ad7329723f215a99675907717b7b7644c28c
Aleksandr Voron <aleksandr.voron@intel.com>
2025-12-16 16:08:23 +0100

    [ARM][FORK] Resolve float32_t type on 32-bit platforms

--- src/cpu/acl/matmul/acl_lowp_matmul_sq.cpp
+++ src/cpu/acl/matmul/acl_lowp_matmul_sq.cpp
@@ -19,6 +19,7 @@
 #include "arm_compute/core/utils/quantization/AsymmHelpers.h"
 
 #include "cpu/acl/acl_utils.hpp"
+#include "src/cpu/CpuTypes.h"
 
 namespace dnnl {
 namespace impl {
@@ -278,10 +279,10 @@ status_t acl_lowp_matmul_sq_t::execute(const exec_ctx_t &ctx) const {
     if (with_bias) {
         auto bia_s32_base = scratchpad.get<uint32_t>(
                 memory_tracking::names::key_conv_bias_s32_convert);
-        auto bia_f32_base = CTX_IN_MEM(const float32_t *, DNNL_ARG_BIAS);
+        auto bia_f32_base = CTX_IN_MEM(const arm_compute::float32_t *, DNNL_ARG_BIAS);
         const float bias_scale = 1 / (*src_scale * (*wei_scale));
         const int num_elements
-                = almc.bia_tensor_info.total_size() / sizeof(float32_t);
+                = almc.bia_tensor_info.total_size() / sizeof(arm_compute::float32_t);
         parallel_nd(num_elements, [&](dim_t e) {
             const auto b = int32_t(std::round(bia_f32_base[e] * bias_scale));
             bia_s32_base[e] = b;
