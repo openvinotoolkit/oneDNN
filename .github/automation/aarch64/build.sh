2dbae8e892592bf11043622d571dbac5a6b720f8
Aleksandr Voron <aleksandr.voron@intel.com>
2025-12-16 16:07:40 +0100

    [FORK][ARM][FIX] Fix ACL configuration and skip failed tests

--- .github/automation/aarch64/build.sh
+++ .github/automation/aarch64/build.sh
@@ -37,7 +37,7 @@ if [[ "$ONEDNN_ACTION" == "configure" ]]; then
         CC=clang CXX=clang++ \
         cmake \
             -Bbuild -S. \
-            -DDNNL_AARCH64_USE_ACL=ON \
+            -DDNNL_USE_ACL=ON \
             -DONEDNN_BUILD_GRAPH=ON \
             -DDNNL_CPU_RUNTIME=OMP \
             -DONEDNN_WERROR=ON \
@@ -50,7 +50,7 @@ if [[ "$ONEDNN_ACTION" == "configure" ]]; then
         set -x
         cmake \
             -Bbuild -S. \
-            -DDNNL_AARCH64_USE_ACL=ON \
+            -DDNNL_USE_ACL=ON \
             -DONEDNN_BUILD_GRAPH=$ONEDNN_BUILD_GRAPH \
             -DDNNL_CPU_RUNTIME=$ONEDNN_THREADING \
             -DONEDNN_WERROR=ON \
